d150d11478e7a678527e29694c2d5f4fe7f2262f
Stephan Wittig <stephan.wittig@cgi.com>
2023-12-18 10:03:32 +0100

    draft part 2

--- day_14_1_wip.py
+++ day_14_1_wip.py
@@ -1,20 +1,25 @@
 from functools import cache
 
-def transpose(platform: list[str]) -> list[str]:
-    """Transpose the matrix, swapping north to east"""
-    return ["".join(t) for t in zip(*platform)]
+def serialize(platform: list[str]) -> str:
+    return "\n".join(platform)
 
-def rotateAntiClockwise(platform: list[str]) -> list[str]:
+def deserialize(platform: str) -> list[str]:
+    return platform.split("\n")
+
+def rotateAntiClockwise(platform: str) -> str:
     """Rotate matrix, turing north to west"""
+    platform = deserialize(platform)
     width = len(platform)
-    return ["".join([platform[x][-y] for x in range(width)]) for y in range(1, width + 1)]
+    return serialize(["".join([platform[x][-y] for x in range(width)]) for y in range(1, width + 1)])
 
-def rotate(platform: list[str]) -> list[str]:
+def rotate(platform: str) -> str:
     """Rotate matrix, turning north to east"""
+    platform = deserialize(platform)
     width = len(platform)
-    return ["".join([platform[-x][y] for x in range(1, width + 1)]) for y in range(width)]
+    return serialize(["".join([platform[-x][y] for x in range(1, width + 1)]) for y in range(width)])
 
-def spinCycle(platform: list[str]) -> list[str]:
+@cache
+def spinCycle(platform: str) -> str:
     """Tilts and rotates platform four times"""
     newPlatform = platform
     for i in range(4):
@@ -22,9 +27,11 @@ def spinCycle(platform: list[str]) -> list[str]:
         newPlatform = rotate(newPlatform)
     return newPlatform
 
-def tiltPlatform(platform: list[str]) -> list[str]:
+def tiltPlatform(platform: str) -> str:
     """Slides all rocks to the beginning of their strings, i.e., left"""
-    return [tiltRow(col) for col in platform]
+    platform = deserialize(platform)
+    platform = list(map(tiltRow, platform))
+    return serialize(platform)
 
 @cache
 def tiltRow(row: str) -> str:
@@ -45,22 +52,22 @@ def findNewPos(row: str, oldPos: int) -> int:
             return i + 1
     return 0
 
-def countLoad(platform: list[str]) -> int:
+def countLoad(platform: str) -> int:
     """Count load on east support beam"""
     load = 0
-    for i, row in platform:
+    for i, row in deserialize(platform):
         for j, char in enumerate(row):
             if char == "O":
                 score += len(row) - i
     return score
 
-platform = open("input/day_14.txt").read().split("\n")
+platform = open("input/day_14.txt").read()
 # Rotate to start strings at north, turning north to east for the following
 platform = rotateAntiClockwise(platform)
 
 cycles = 1000000000
+lastPlatform = platform
 for i in range(cycles):
-    print(f"Progress: {i/cycles * 100}%")
+    print(f"Progress: {round(i/cycles * 100, 2)}%", end = "\r")
     platform = spinCycle(platform)
-    #print("\n".join(rotate(platform)) + "\n")
 print(f"Load: {countLoad(platform)}")
